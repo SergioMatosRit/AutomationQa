fff2775ff638821976fdd7c034b01095969a7ec8
Sérgio <sergio.matos@readinessit.com>
2019-11-21 16:56:20 +0000

    #1st commit

--- src/pages/Home.java
+++ src/pages/Home.java
@@ -1,4 +1,24 @@
 package pages;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import java.util.List;
 
 public class Home {
+    WebDriver driver;
+
+
+    By SearchButton = By.xpath("//div[@class='search']/div/button");
+
+    public Home(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void SearchBox() {
+        WebElement button = driver.findElement(SearchButton);
+        button.click();
+    }
+
+
+
 }
